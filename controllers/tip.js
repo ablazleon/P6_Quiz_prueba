8c2adf8c66ebf5d8dee597d19a4ed5b29db6402c
Adrian <ablazleon@ono.com>
2018-05-25 08:40:00 +0100

    practica8

--- controllers/tip.js
+++ controllers/tip.js
@@ -75,3 +75,37 @@ exports.destroy = (req, res, next) => {
     .catch(error => next(error));
 };
 
+// GET /quizzes/:quizId/edit
+exports.edit = (req, res, next) => {
+
+    const {tip, quiz} = req;
+    // const tip = req.tip;
+    // const quiz = req.quiz;
+
+    res.render('quizzes/edit', {tip, quiz});
+};
+
+
+// PUT /quizzes/:quizId/tips/:tipId
+exports.update = (req, res, next) => {
+
+    const {quiz, body} = req;
+
+    quiz.question = body.question;
+    quiz.answer = body.answer;
+
+    quiz.save({fields: ["question", "answer"]})
+        .then(quiz => {
+        req.flash('success', 'Quiz edited successfully.');
+    res.redirect('/quizzes/' + quiz.id);
+})
+.catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+    error.errors.forEach(({message}) => req.flash('error', message));
+    res.render('quizzes/edit', {quiz});
+})
+.catch(error => {
+        req.flash('error', 'Error editing the Quiz: ' + error.message);
+    next(error);
+});
+};
